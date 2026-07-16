cc3b56874d9351a95d5578579b8d6fb779567c17
Shivam Patel <hsmash715@gmail.com>
2026-07-16 12:49:06 +0530

    Number Guessing Game

---
+++ Number Guessing.py
@@ -0,0 +1,13 @@
+import random
+number=random.randint(0,100)
+attempts=0
+while True:
+    guess=int(input("Guess A Number Between 1-100="))
+    attempts+=1
+    if guess<number:
+        print("Low")
+    elif guess>number:
+        print("High")
+    else:
+        print("Your Guess is Right,You done in",attempts,"Attempts")
+        break
