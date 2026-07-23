9e7f327cf9d5b36fda6f597d1cfb5ac8c5b5fc65
Shivam Patel <hsmash715@gmail.com>
2026-07-23 22:13:35 +0530

    Change in Number Guessing

--- Number Guessing.py
+++ Number Guessing.py
@@ -1,13 +1,15 @@
 import random
-number=random.randint(0,100)
-attempts=0
-while True:
-    guess=int(input("Guess A Number Between 1-100="))
-    attempts+=1
-    if guess<number:
-        print("Low")
-    elif guess>number:
-        print("High")
-    else:
-        print("Your Guess is Right,You done in",attempts,"Attempts")
-        break
+def Guessing():
+    number=random.randint(0,100)
+    attempts=0
+    while True:
+        guess=int(input("Guess A Number Between 1-100="))
+        attempts+=1
+        if guess<number:
+            print("Low")
+        elif guess>number:
+            print("High")
+        else:
+            print("Your Guess is Right,You done in",attempts,"Attempts")
+            break
+Guessing()
