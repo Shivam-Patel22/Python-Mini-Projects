41eb925cb92ddd8a118479cbcc896c72565a6464
Shivam Patel <hsmash715@gmail.com>
2026-07-24 12:42:55 +0530

    Change in the Apperance of the Output for better Understanding

--- Tempertaure Conveter.py
+++ Tempertaure Conveter.py
@@ -3,32 +3,44 @@ class Converter():
         #1.Celsuis to Kelvin
         self.celsius=float(input("Enter Celsius="))
         self.kelvin=self.celsius+273.15
+        print("="*40)
         print("Kelvin=",self.kelvin)
+        print("="*40)
     def Cel_Kel(self):
         #Kelvin to Celsuis
         self.kelvin=float(input("Enter Kelvin="))
         self.celsius=self.kelvin-273.15
+        print("="*40)
         print("Celsius=",self.celsius)
+        print("="*40)
     def Kel_far(self):
         #Fareheit to Kelvin
         self.fareheit=float(input("Enter Farenheit="))
         self.kelvin=(self.fareheit+459.67)*5/9
+        print("="*40)
         print("Kelvin=",self.kelvin)
+        print("="*40)
     def Far_kel(self):
         #Kelvin to Fareheit
         self.kelvin=float(input("Enter Kelvin="))
         self.fareheit=(self.kelvin*5/9)-459.67
+        print("="*40)
         print("Fareheit=",self.fareheit)
+        print("="*40)
     def Far_cal(self):
         #Fareheit to Celsuis
         self.celsuis=float(input("Enter Celsius="))
         self.fareheit=(self.celsuis*9/5)+32
+        print("="*40)
         print("Fareheit=",self.fareheit)
+        print("="*40)
     def Cal_far(self):
         #Celsuis to Fareheit
         self.fareheit=float(input("Enter Farenheit="))
         self.celsuis=(self.fareheit-32)/(9/5)
+        print("="*40)
         print("Celsuis=",self.celsuis)
+        print("="*40)
 x=1
 c=Converter()
 while x!=7:
@@ -36,8 +48,8 @@ while x!=7:
     print("2.Kelvin to Celsuis")
     print("3.Fareheit to Kelvin")
     print("4.Kelvin to Fareheit")
-    print("5.Fareheit to Celsuis")
-    print("6.Celsuis to Fareheit")
+    print("5.Celsuis to Fareheit")
+    print("6.Fareheit to Celsuis")
     print("7.EXIT")
     x=int(input("Enter Your Choice From 1 to 6:"))
     match x:
